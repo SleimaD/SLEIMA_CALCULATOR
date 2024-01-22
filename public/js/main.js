5259c00e944e6155dced6be28988067de81f8136
SleimaD <sleima@icloud.com>
2024-01-22 15:27:37 +0100

    final version

--- public/js/main.js
+++ public/js/main.js
@@ -4,30 +4,57 @@ let output = document.querySelector('.result');
 //? Get all the buttons
 let buttons = document.querySelectorAll('.bouton, .operator, .equal, .clear');
 
+let last = false
+
+
 //? for click events on the buttons
 buttons.forEach(button => {
- button.addEventListener('click', () => {
-    //? Get the value of the button
-    let value = button.innerText;
 
-    //? Perform the corresponding operation
-    switch (value) {
-      case 'C':
-        //? Clear the output
-        output.innerText = '';
-        break;
-      case '=':
-        //? Evaluate the expression in the output
-        try {
-          output.innerText = Function("return " + output.innerText)();
-        } catch (error) {
-          output.innerText = 'Error';
+    button.addEventListener('click', () => {
+
+        //? Get the value of the button
+        let value = button.innerText;
+
+
+        if (output.innerText === '' && value !== '-' && ["+", "-", "*", "/", "%"].includes(value)) {
+            return;
+        } //? to prevent from starting with an operator ( except '-' )
+
+
+        //? If the last character of the output is an operator and the clicked button is also an operator, return 
+        if (["+", "-", "*", "/", "%"].includes(output.innerText.slice(-1)) && ["+", "-", "*", "/", "%"].includes(value)) {
+            return;
+        }else if (last) {
+            if (["+", "-", "*", "/", "%"].includes(value)) {
+                last = false
+                output.innerText += value
+            }else{
+                last  = false
+                output.innerText = button.value
+            }
         }
-        break;
-      default:
-        //? add the button value to the output
-        output.innerText += value;
-    }
- });
- 
+
+
+        //? Perform the corresponding operation
+        switch (value) {
+            case 'C':
+                //? Clear the output
+                output.innerText = '';
+                break;
+            case '=':
+                //? Evaluate the expression in the output
+                try {
+                    output.innerText = Function("return " + output.innerText)();
+                    last = true 
+                } catch (error) {
+                    output.innerText = 'Error';
+                }
+                break;
+            default:
+                //? add the button value to the output
+                output.innerText += value;
+        }
+
+    });
+
 });
